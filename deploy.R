30e3a43b67bbc8c5b356a9e3c43559672b33d130
mdka00001 <mdka00001@stud.uni-saarland.de>
2023-10-04 19:33:07 +0200

    shiny update

--- deploy.R
+++ deploy.R
@@ -1,6 +1,6 @@
 R.version.string
 
-install.packages("rsconnect", dependencies=TRUE, repos="https://cran.r-project.org/src/contrib/rsconnect_1.1.1.tar.gz")
+install.packages("rsconnect", dependencies=TRUE, repos=repos='http://cran.rstudio.com/')
 library(rsconnect)
 
 # a function to stop the script when one of the variables cannot be found
