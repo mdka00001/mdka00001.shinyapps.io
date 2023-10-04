450c176ddbced01da47468db7285aecc044e6225
mdka00001 <mdka00001@stud.uni-saarland.de>
2023-10-04 18:40:30 +0200

    shiny update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM rocker/shiny:4.2.1
 
-RUN R -e "install.packages('rsconnect')"
-RUN install2.r rsconnect tibble dplyr stringr rtweet htmltools lubridate bslib reactable
+RUN R -e "install.packages(c('rsconnect','data.table','mixtox','ggplot2','gridExtra','shinyscreenshot','bayestestR','BAS','markdown','knitr','readr','stringr'))"
+RUN install2.r rsconnect tibble dplyr stringr rtweet htmltools lubridate bslib reactable data.table mixtox ggplot2 gridExtra shinyscreenshot bayestestR BAS markdown knitr readr stringr
 WORKDIR /home/shinytweet
 COPY ui.R ui.R 
 COPY server.R server.R
--- deploy.R
+++ deploy.R
@@ -1,5 +1,17 @@
-update.packages(c("rsconnect"))
-rsconnect::setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
-               token = Sys.getenv("TOKEN"),
-               secret = Sys.getenv("SECRET"))
+library(rsconnect)
+
+# a function to stop the script when one of the variables cannot be found
+# and to strip quotation marks from the secrets when you supplied them
+error_on_missing_name <- function(name) {
+  var <- Sys.getenv(name, unset = NA)
+  if(is.na(var)) {
+    stop(paste0("cannot find ", name, " !"), call. = FALSE)
+  }
+  gsub("\"", "", var)
+}
+
+# Authenticate
+setAccountInfo(name = error_on_missing_name("SHINY_ACC_NAME"),
+               token = error_on_missing_name("TOKEN"),
+               secret = error_on_missing_name("SECRET"))
 deployApp(appFiles = c("ui.R", "server.R"))
