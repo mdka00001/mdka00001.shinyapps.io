27483e189f2a61a206540650dc479d4f8edf6dea
mdka00001 <mdka00001@stud.uni-saarland.de>
2023-10-04 18:53:35 +0200

    shiny update

--- deploy.R
+++ deploy.R
@@ -12,7 +12,7 @@ error_on_missing_name <- function(name) {
 }
 
 # Authenticate
-setAccountInfo(name = error_on_missing_name("SHINY_ACC_NAME"),
-               token = error_on_missing_name("TOKEN"),
-               secret = error_on_missing_name("SECRET"))
+rsconnect::setAccountInfo(name='mdka00001',
+			  token='90B6D77B475D0DF86B202B32CB0C6837',
+			  secret='k/PwfavfidHS0e8FtjAPje08upQU+PqQOY0LOj0o')
 deployApp(appFiles = c("ui.R", "server.R"))
